de110bb004219b2df452c2aa25fef9a127a00935
AndreiLucas123 <andrei.lucas.goncalves@gmail.com>
2024-08-24 09:13:37 -0300

    Using tsx with esbuild, and fixed logTime tiping

--- src/utils/logTime.ts
+++ src/utils/logTime.ts
@@ -6,7 +6,7 @@
  * @param label The label to display in the console
  * @param cb The function to execute
  */
-export async function logTime(label: string, cb: () => Promise<void>) {
+export async function logTime(label: string, cb: () => Promise<any>) {
   label = '\x1b[34m' + label + '\x1b[0m';
   console.time(label);
 
